61a4de839ad12f8633a1b16fc4cc12498734ab91
ben <75618212+bazpxl@users.noreply.github.com>
2024-08-10 07:13:39 +0200

    removed redundant parts in tilemap.h and moved function definitions to cpp file

--- src/tilemap.cpp
+++ src/tilemap.cpp
@@ -3,6 +3,27 @@
 #include <filesystem>
 using namespace BzlGame;
 
+void Map::setTile(const u8 layer, const u16 id, const Tile tile) {
+	DebugOnly(
+		assert(tiles_.size() > layer);
+		assert(tiles_[layer].size() > id);
+	)
+	tiles_[layer][id] = tile;
+}
+
+void Map::AddLayer() {
+	tiles_.push_back(Vector<Tile>(rows_*cols_, {EmptyTileVal, 0}));
+	layer_number_++;
+}
+
+void Map::RemoveLayer(const int id) {
+	if(id < tiles_.size())
+	{
+		tiles_.erase(tiles_.begin() + id);
+		layer_number_--;
+	}
+}
+
 Vector<string> Map::ReadJson(const string & path)
 {
 	std::ifstream file(path);
@@ -16,12 +37,10 @@ Vector<string> Map::ReadJson(const string & path)
 	rows_							=	j["rows"].template get<u16>();
 	cols_							=	j["cols"].template get<u16>();
 	tilesize_						=	j["tilesize"].template get<u8>();
-	layer_number_						=	j["layer_numb"].template get<u8>();
+	layer_number_					=	j["layer_number"].template get<u8>();
 	tiles_							=	j["tiles"].template get<Vector<Vector<Tile>>>();
 
-
 	file.close();
-
 	return asset_paths_;
 }
 
@@ -37,7 +56,7 @@ void Map::WriteJson(const string & path)
 			{"rows", rows_},
 			{"cols", cols_},
 			{"tilesize", tilesize_},
-			{ "layer_numb", layer_number_},
+			{ "layer_number", layer_number_},
 			{"tiles", tiles_}
 	};
 
@@ -48,6 +67,18 @@ void Map::WriteJson(const string & path)
 	}
 	file <<  dataJson << std::endl;
 	file.close();
+}
+
+void Map::CreateAssets(const Vector<string>& asset_paths, Renderer* render) {
+	for(auto & vec : asset_paths)
+	{
+		string tmpstr = BasePath;
+		tmpstr.append(vec);
+		SharedPtr<Texture> texture_shptr = CreateSharedTexture(render, tmpstr.c_str());
+		Point size;
+		SDL_QueryTexture(texture_shptr.get(), nullptr, nullptr, &size.x, &size.y );
+		tilesets_.push_back({texture_shptr , size, vec});
+	}
 }
 
 
--- src/tilemap.h
+++ src/tilemap.h
@@ -33,7 +33,7 @@ private:
 	static constexpr u16 TileSize		= 32;
 
 	Vector<TileSet> tilesets_;
-	Vector<Vector<Tile>> tiles_ {LayerNumb-1, Vector<Tile>(MapRows*MapCols,{EmptyTileVal,0})};
+	Vector<Vector<Tile>> tiles_;
 
 	u16 rows_{};
 	u16 cols_{};
@@ -42,13 +42,13 @@ private:
 public:
 	// ctor empty map without tilesets
 	explicit Map(const u16 rows = MapRows,		const u16 cols	= MapCols,
-				 const u16 tilesize = TileSize, const u8  layer_numb = LayerNumb)
+				 const u16 tilesize = TileSize, const u8  layer_number = LayerNumber)
 		: rows_(rows), cols_(cols),
-		  tilesize_(tilesize), layer_number_(layer_numb)
+		  tilesize_(tilesize), layer_number_(layer_number)
 	{
 		tiles_ =
 		{
-			static_cast <size_t>(layer_numb),
+			static_cast <size_t>(layer_number),
 			Vector<Tile>(rows*cols,
 			{EmptyTileVal,0})
 		};
@@ -58,36 +58,25 @@ public:
 	// create map and read data from file
 	explicit Map( const string & path, Renderer* render)
 		: rows_(MapRows), cols_(MapCols),
-		  tilesize_(TileSize), layer_number_(LayerNumb)
+		  tilesize_(TileSize), layer_number_(LayerNumber)
 	{
 		tiles_ =	{static_cast <size_t>(layer_number_)-1, Vector<Tile>(rows_*cols_,{EmptyTileVal,0})};
-		Vector<string> asset_paths_ = ReadJson(path);
-		CreateAssets(asset_paths_, render);
+		Vector<string> asset_paths = ReadJson(path);
+		CreateAssets(asset_paths, render);
 	}
 
-
-
-	void AddTileset(const SharedPtr<Texture> & texture,const Point size, const string& path)
-	{
-		tilesets_.push_back({texture, size, path});
-		layer_number_++;
-	}
+	void AddTileset(const SharedPtr<Texture> & texture,const Point size, const string& path){	tilesets_.push_back({texture, size, path});	}
 
 	void RemoveTileset(const int id)
 	{
 		assert(tilesets_.size() > id);
 		tilesets_.erase(tilesets_.begin() + id);
-		layer_number_ = static_cast<u8>(tilesets_.size());
 	}
 
-	void setTile(const u8 layer, const u16 id, const Tile tile)
-	{
-		DebugOnly(
-			assert(tiles_.size() > layer);
-			assert(tiles_[layer].size() > id);
-			)
-		tiles_[layer][id] = tile;
-	}
+	void setTile(const u8 layer, const u16 id, const Tile tile);
+
+	void AddLayer();
+	void RemoveLayer(const int id);
 
 	Vector<string> ReadJson(const string & path);
 	void WriteJson(const string & path);
@@ -95,7 +84,7 @@ public:
 	void CreateAssets(const Vector<string>& asset_paths, Renderer* render);
 
 	[[nodiscard]] Vector<Tile>			&			getLayer(const int index)			 {	return tiles_.at(index);	}
-	[[nodiscard]] Vector<Vector<Tile>>	&			getTileVec()							 {	return tiles_;				}
+	[[nodiscard]] Vector<Vector<Tile>>	&			getTileVec()						 {	return tiles_;				}
 	[[nodiscard]] Vector<TileSet>		&			getTilesets()						 {	return tilesets_;			}
 	[[nodiscard]] TileSet				&			getTileset(const int id)			 {	return tilesets_[id];		}
 	[[nodiscard]] Point								getTilesetSize(const int id)	const{	return tilesets_[id].size;	}
@@ -103,10 +92,9 @@ public:
 	[[nodiscard]] u16								rows()							const{	return rows_;				}
 	[[nodiscard]] u16								cols()							const{	return cols_;				}
 	[[nodiscard]] u16								tilesize()						const{	return tilesize_;			}
-	[[nodiscard]] u8								layer_number()					const{	return layer_number_;			}
+	[[nodiscard]] u8								layer_number()					const{	return layer_number_;		}
 
 };
-
 
 //--------------------------------------------------
 
